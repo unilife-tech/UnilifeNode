7fb7752b5785b33525f47ff2a10d05c163707a8d
Alwan Kazi <alwan@trusity.com>
2021-08-19 21:28:39 +0400

    Update

--- models/university.js
+++ models/university.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
@@ -22,29 +21,4 @@ var universitychema = new Schema({
   }, { collection: 'university_tbl' });
   
   module.exports = mongoose.model('University', universitychema);
-=======
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-
-var universitychema = new Schema({
-    
-   
-    university_name: {
-      type: String,
-      required: false
-    },
-    university_id: {
-      type: String,
-      required: true
-    },
-    domain: {
-      type: String,
-      required: false
-    },
-
-
-  }, { collection: 'university_tbl' });
-  
-  module.exports = mongoose.model('University', universitychema);
->>>>>>> 5f8eddb822302c32a8fe780ee475d117b7c87718
   
